faa3986f1b52432b797c4b84131fb50e78f6f3be
harlenVII <harlanxu16@gmail.com>
2026-05-25 02:28:15 -0700

    feat: add onHover/onLeave props to WindPanel, remove floating Tooltip

--- components/WindPanel.tsx
+++ components/WindPanel.tsx
@@ -6,7 +6,6 @@ import {
   Legend,
   Line,
   ResponsiveContainer,
-  Tooltip,
   XAxis,
   YAxis,
 } from "recharts";
@@ -15,16 +14,26 @@ interface WindPanelProps {
   data: { x: string; speed: number; gust: number }[];
   ticks?: string[];
   tickFormatter?: (v: string) => string;
+  onHover?: (index: number) => void;
+  onLeave?: () => void;
 }
 
-export function WindPanel({ data, ticks, tickFormatter }: WindPanelProps) {
+export function WindPanel({ data, ticks, tickFormatter, onHover, onLeave }: WindPanelProps) {
   return (
     <ResponsiveContainer width="100%" height={160}>
-      <ComposedChart data={data} margin={{ top: 8, right: 80, bottom: 16, left: 0 }}>
+      <ComposedChart
+        data={data}
+        margin={{ top: 8, right: 80, bottom: 16, left: 0 }}
+        onMouseMove={(state) => {
+          const idx = state.activeTooltipIndex;
+          if (typeof idx === "number" && onHover) onHover(idx);
+        }}
+        onMouseLeave={onLeave}
+        onTouchEnd={onLeave}
+      >
         <CartesianGrid strokeDasharray="3 3" stroke="#eee" />
         <XAxis dataKey="x" ticks={ticks} tickFormatter={tickFormatter} />
         <YAxis label={{ value: "m/s", angle: -90, position: "insideLeft" }} />
-        <Tooltip labelFormatter={(v) => String(v).replace("T", " ")} />
         <Legend />
         <Bar dataKey="gust" name="Gust (m/s)" fill="#6ee7b7" fillOpacity={0.6} />
         <Line dataKey="speed" name="Speed (m/s)" stroke="#059669" strokeWidth={2} dot={false} />
